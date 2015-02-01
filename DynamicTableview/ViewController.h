51b9b4337d30c184a709eb383f239017acba7d0d
Roshit Gopinath <roshh@Roshits-Mac-Pro.local>
2015-02-01 11:42:50 -0800

    Initial Commit

---
+++ DynamicTableview/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DynamicTableview
+//
+//  Created by Roshit Gopinath on 2/1/15.
+//  Copyright (c) 2015 Roshit Gopinath. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ DynamicTableview/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  DynamicTableview
+//
+//  Created by Roshit Gopinath on 2/1/15.
+//  Copyright (c) 2015 Roshit Gopinath. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
